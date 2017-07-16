fd04e1ffe862e53d6a2b85ab6fbd49c1e26936de
Prince Park <prince_ppy@yahoo.com>
2017-07-16 14:32:12 +0400

    first commit

---
+++ dist/angular-sharepoint-rest-api.js
@@ -0,0 +1,139 @@
+(function () {
+    'use strict';
+
+    angular.module('spNgModule')
+        .directive("customFileChange", customFileChange)
+})();
+(function () {
+    'use strict';
+
+    angular.module('spNgModule')
+        .factory("$spBaseService", SPBaseService);
+
+    SPBaseService.$inject = ["$q", "$http", "$spNgModuleConfig"];
+
+    function SPBaseService($q, $http, $spNgModuleConfig) {
+
+        var baseUrl = $spNgModuleConfig.isAppWeb ? _spPageContextInfo.webAbsoluteUrl : _spPageContextInfo.siteAbsoluteUrl;
+
+        return {
+            getRequest: getRequest,
+            postRequest: postRequest,
+            updateRequest: updateRequest,
+            deleteRequest: deleteRequest,
+            fileUploadRequest: fileUploadRequest,
+            baseUrl: baseUrl
+        };
+
+        function getRequest(query, endPoint) {
+            var deferred = $q.defer();
+            $http({
+                url: endPoint || baseUrl + query,
+                method: "GET",
+                headers: {
+                    "accept": "application/json;odata=verbose",
+                    "content-Type": "application/json;odata=verbose"
+                }
+            }).success(function (result) {
+                deferred.resolve(result);
+            }).error(function (result, status) {
+                deferred.reject({
+                    error: result,
+                    status: status
+                });
+            });
+            return deferred.promise;
+        }
+
+        function postRequest(data, url, endPoint) {
+            var deferred = $q.defer();
+            $http({
+                url: endPoint || baseUrl + url,
+                method: "POST",
+                headers: {
+                    "accept": "application/json;odata=verbose",
+                    "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
+                    "content-Type": "application/json;odata=verbose"
+                },
+                data: JSON.stringify(data)
+            }).success(function (result) {
+                deferred.resolve(result);
+            }).error(function (result, status) {
+                deferred.reject({
+                    error: result,
+                    status: status
+                });
+            });
+            return deferred.promise;
+        }
+
+        function updateRequest(data, url, endPoint) {
+            var deferred = $q.defer();
+            $http({
+                url: endPoint || baseUrl + url,
+                method: "PATCH",
+                headers: {
+                    "accept": "application/json;odata=verbose",
+                    "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
+                    "content-Type": "application/json;odata=verbose",
+                    "X-Http-Method": "PATCH",
+                    "If-Match": "*"
+                },
+                data: JSON.stringify(data)
+            }).success(function (result) {
+                deferred.resolve(result);
+            }).error(function (result, status) {
+                deferred.reject({
+                    error: result,
+                    status: status
+                });
+            });
+            return deferred.promise;
+        }
+
+        function deleteRequest(url, endPoint) {
+            var deferred = $q.defer();
+            $http({
+                url: endPoint || baseUrl + url,
+                method: "DELETE",
+                headers: {
+                    "accept": "application/json;odata=verbose",
+                    "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
+                    "IF-MATCH": "*"
+                }
+            }).success(function (result) {
+                deferred.resolve(result);
+            }).error(function (result, status) {
+                deferred.reject({
+                    error: result,
+                    status: status
+                });
+            });
+            return deferred.promise;
+        }
+
+        function fileUploadRequest(data, url, endPoint) {
+            var deferred = $q.defer();
+            $http({
+                url: endPoint || baseUrl + url,
+                method: "POST",
+                processData: false,
+                data: data,
+                transformRequest: angular.identity,
+                headers: {
+                    "accept": "application/json;odata=verbose",
+                    "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
+                    "Content-Type": undefined
+                }
+            }).success(function (result) {
+                deferred.resolve(result);
+            }).error(function (result, status) {
+                deferred.reject({
+                    error: result,
+                    status: status
+                });
+            });
+            return deferred.promise;
+        }
+    } 
+})();
