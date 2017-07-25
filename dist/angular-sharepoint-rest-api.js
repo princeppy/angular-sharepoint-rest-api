a9c657faf2ebcef1a338a78adaa94e5025a8a049
Prince Park <prince_ppy@yahoo.com>
2017-07-25 13:07:29 +0400

    Update ANd corrected SPListService

--- dist/angular-sharepoint-rest-api.js
+++ dist/angular-sharepoint-rest-api.js
@@ -419,22 +419,13 @@
     angular.module('spNgModule')
         .factory('$spListService', SPListService);
 
-<<<<<<< HEAD
     SPListService.$inject = ['$q', '$http', '$spNgModuleConfig', '$document', '$spConvertService', '$timeout'];
     function SPListService($q, $http, $spNgModuleConfig, $document, $spConvert, $timeout) {
-=======
-    SPListService.$inject = ['$q', '$http', '$spNgModuleConfig', '$document', '$spConvertService'];
-    function SPListService($q, $http, $spNgModuleConfig, $document, $spConvert) {
-        var $spListMinErr = angular.$$minErr('$spList');
-        var baseUrl = _spPageContextInfo.webAbsoluteUrl;
-        var baseAPIUrl = baseUrl.trimEnd('/') + '/_api/web/lists/';
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
 
         String.prototype.trimStart = function (c) { if (c === undefined) { c = '\s'; } return this.replace(new RegExp('^[' + c + ']+'), ''); };
         String.prototype.trimEnd = function (c) { if (c === undefined) { c = '\s'; } return this.replace(new RegExp('[' + c + ']+$'), ''); };
         String.prototype.trim = function (c) { return this.trimStart(c).trimEnd(c); };
 
-<<<<<<< HEAD
         var $spListMinErr = angular.$$minErr('$spList');
         var baseUrl = _spPageContextInfo.webAbsoluteUrl;
         var baseAPIUrl = baseUrl.trimEnd('/') + '/_api/web/lists/';
@@ -450,17 +441,10 @@
          */
         var listFactoryObject = function (title, listOptions) {
             // Public properties, assigned to the instance ('this')
-=======
-        /*
-        * Constructor function
-        */
-        var listFactoryObject = function (title, listOptions) {
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
             if (!angular.isString(title) || title === '') {
                 throw $spListMinErr('badargs', 'title must be a nen-empty string.');
             }
 
-<<<<<<< HEAD
             var thisList = Object.create(this);
 
             thisList.baseUrl = baseUrl;
@@ -470,23 +454,13 @@
             thisList.listOptions = angular.extend({}, listOptions || {});
 
             thisList.normalizedTitle = $spConvert.capitalize(thisList.title
-=======
-            this.title = title;
-            this.listOptions = angular.extend({}, listOptions || {});
-
-            this.baseUrl = baseUrl;
-            this.baseAPIUrl = baseAPIUrl;
-
-            this.normalizedTitle = $spConvert.capitalize(this.title
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
                 .replace(/[^A-Za-z0-9 ]/g, '')      // remove invalid chars
                 .replace(/\s/g, '_x0020_')          // replace whitespaces with _x0020_
             );
-            this.className = $spConvert.capitalize(this.normalizedTitle
+            thisList.className = $spConvert.capitalize(thisList.normalizedTitle
                 .replace(/_x0020/g, '')             // remove _x0020_
                 .replace(/^\d+/, '')                // remove leading digits
             );
-<<<<<<< HEAD
             thisList.listItemType = 'SP.Data.' + thisList.normalizedTitle + 'ListItem';
             thisList.__metadata = { type: thisList.listItemType };
             thisList.Url = baseAPIUrl + 'GetByTitle(\'' + thisList.title + '\')';
@@ -562,59 +536,13 @@
                 thisList.Fields = response.data.d.Fields.results
                     .filter(function (item) { return item.ReadOnlyField === false; })
                     .map(function (item) {
-=======
-            this.listItemType = 'SP.Data.' + this.normalizedTitle + 'ListItem';
-            this.__metadata = { type: this.listItemType };
-            this.Url = baseAPIUrl + 'GetByTitle(\'' + this.title + '\')';
-        };
-
-        /*
-        * Prototypal setup
-        */
-        listFactoryObject.prototype = {
-            init: function () {
-                var listUrl = this.Url + '?$expand=ContentTypes';
-                listUrl += ',Fields';
-                listUrl += ',Items,Items/AttachmentFiles';
-                this.listUrl = listUrl;
-
-                var listHeader = {
-                    'Accept': 'application/json;odata=verbose',
-                    'Content-Type': 'application/json;odata=verbose',
-                    'X-RequestDigest': $('#__REQUESTDIGEST').val(),
-                    'X-HTTP-Method': 'GET'
-                };
-
-                var httpPromise = $http({ url: listUrl, method: 'GET', headers: listHeader });
-
-                //transform response
-                var derivedPromise = httpPromise.then(function (response) {
-                    console.log('results from => ', listUrl);
-                    this.Url = response.data.d.__metadata.uri;
-                    this.etag = response.data.d.__metadata.etag.toString();
-                    this.AllowContentTypes = response.data.d.AllowContentTypes;
-                    this.BaseTemplate = response.data.d.BaseTemplate;
-                    this.ContentTypesEnabled = response.data.d.ContentTypesEnabled;
-                    this.EnableAttachments = response.data.d.EnableAttachments;
-                    this.EnableFolderCreation = response.data.d.EnableFolderCreation;
-                    this.EnableMinorVersions = response.data.d.EnableMinorVersions;
-                    this.EnableModeration = response.data.d.EnableModeration;
-                    this.EnableVersioning = response.data.d.EnableVersioning;
-                    this.EntityTypeName = response.data.d.EntityTypeName;
-                    this.Hidden = response.data.d.Hidden;
-                    this.Id = response.data.d.Id;
-                    this.ItemCount = response.data.d.ItemCount;
-                    this.ParentWebUrl = response.data.d.ParentWebUrl;
-                    this.Title = response.data.d.Title;
-                    this.ListItemEntityTypeFullName = response.data.d.ListItemEntityTypeFullName;
-                    this.ContentTypes = response.data.d.ContentTypes.results.map(function (item) {
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
                         return {
-                            'Id': item.Id.StringValue,
-                            'Name': item.Name
+                            'Title': item.Title,
+                            'StaticName': item.StaticName,
+                            'InternalName': item.InternalName,
+                            'Type': item.__metadata.type
                         };
                     });
-<<<<<<< HEAD
                 thisList.Items = [];
                 angular.forEach(response.data.d.Items.results, function (value, key) {
                     value.metadatatype = value.__metadata.type;
@@ -624,19 +552,11 @@
                         var _attachments = [];
                         _attachments = value.AttachmentFiles.results.map(function (fl) {
                             // console.log('fl',fl);
-=======
-                    this.Fields = response.data.d.Fields.results
-                        .filter(function (item) { return item.ReadOnlyField === false; })
-                        .map(function (item) {
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
                             return {
-                                'Title': item.Title,
-                                'StaticName': item.StaticName,
-                                'InternalName': item.InternalName,
-                                'Type': item.__metadata.type
+                                'FileName': fl.FileName,
+                                'FileUrl': _spPageContextInfo.webAbsoluteUrl + '/' + fl.ServerRelativeUrl
                             };
                         });
-<<<<<<< HEAD
                         value.Attachments = _attachments;
                     }
                     this.push(_.omit(value, ['__metadata', 'ID', 'FirstUniqueAncestorSecurableObject', 'RoleAssignments', 'AttachmentFiles', 'ContentType', 'GetDlpPolicyTip', 'FieldValuesAsHtml', 'FieldValuesAsText', 'FieldValuesForEdit', 'File', 'Folder', 'ParentList', 'FileSystemObjectType', 'OData__UIVersionString', 'AuthorId', 'EditorId']));
@@ -724,30 +644,11 @@
             }
 
             var editItemUrl = this.Url + '/Items(' + id + ')';
-=======
-                    this.Items = [];
-                    angular.forEach(response.data.d.Items.results, function (value, key) {
-                        value.metadatatype = value.__metadata.type;
-                        value.uri = value.__metadata.uri;
-                        value.etag = value.__metadata.etag.toString();
-                        if (value.Attachments) {
-                            var _attachments = [];
-                            _attachments = value.AttachmentFiles.results.map(function (fl) {
-                                // console.log('fl',fl);
-                                return {
-                                    'FileName': fl.FileName,
-                                    'FileUrl': _spPageContextInfo.webAbsoluteUrl + '/' + fl.ServerRelativeUrl
-                                };
-                            });
-                            value.Attachments = _attachments;
-                        }
-                        this.push(_.omit(value, ['__metadata', 'ID', 'FirstUniqueAncestorSecurableObject', 'RoleAssignments', 'AttachmentFiles', 'ContentType', 'GetDlpPolicyTip', 'FieldValuesAsHtml', 'FieldValuesAsText', 'FieldValuesForEdit', 'File', 'Folder', 'ParentList', 'FileSystemObjectType', 'OData__UIVersionString', 'AuthorId', 'EditorId']));
-                    }, this.Items);
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
 
-                });
+            if (!_.contains(this.Items, { 'Id': id })) {
+                throw $spListMinErr('badargs', 'item dosen\'t exist in the list.');
+            }
 
-<<<<<<< HEAD
             var data = angular.extend({}, { 'Id': id });
             if (!(data.__metadata)) { data.__metadata = this.__metadata; }
             if (!(data.__metadata.type)) { data.__metadata.type = this.__metadata.type; }
@@ -760,15 +661,6 @@
             });
         };
 
-=======
-                return derivedPromise;
-            },
-            createItem: function (item) { },
-            editItem: function (item) { },
-            deleteItem: function (id) { },
-        }
-
->>>>>>> 5373b9d83cdec5cebcd9e47b7cd7ac0029919a0d
         return listFactoryObject;
         // return function (title, listOptions) {
         //     return function () {
